496bf62f75480abf48642b4e2638a4bd79d21c87
Divyansh Batham <divyansh.sunita@gmail.com>
2017-02-16 00:52:59 +0530

    Added main files to git
    Mainfile.cpp
    Highscores.h
    Movement.h

---
+++ Main Files/Highscores.h
@@ -0,0 +1,86 @@
+#include <time.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+// Saving Highscores:
+struct player
+{
+	char name[18];
+	long int time;
+}score;
+
+void saveHighscore( int diffTime )
+{	
+	FILE *fptr;
+	fptr = fopen("scores.su", "ab");
+	
+	printf("Enter Player Name - ");
+	gets(score.name);  // I need to make name.length <=18
+
+	score.name[18]='\0';
+	score.time = diffTime;
+	fwrite( (char*)&score, 1, sizeof(score),fptr );
+	
+	fclose(fptr);
+}
+
+
+// Printing Highscores:
+
+void paddedName( char name[] )
+{
+	printf(" %-19s", name);
+}
+
+void paddedTime( long int time )
+{
+	int h = time/3600;
+	int m = (time%3600)/60;
+	int s = time%60;
+	printf("   %02d : %02d : %02d   ",h,m,s);
+}
+
+void print(int n,char c)
+{
+	while(n--)	printf("%c",c);
+}
+
+void printHighscores()
+{
+	system("cls");
+	char names[10][18];
+	long int times[10];
+	FILE *fptr;
+	fptr = fopen("scores.su", "rb");
+	
+	cout<<"\n\n\n			HIGH SCORES:";
+	printf("\n	%c",201); print(20,205); printf("%c",209); print(20,205); printf("%c",187);
+	
+	
+	// PRINTING :
+	int count=1;
+	while(fread( (char*)&score, 1, sizeof(score),fptr ))
+	{	
+		printf("\n	%c",186);
+		
+		// Instead of printing, add it to top10 with insertion
+		paddedName(score.name);			// Input from file
+			
+		printf("%c ",179);
+			
+		// Instead of printing, add it to top10 with insertion
+		paddedTime(score.time);			// Input from file
+		
+		printf(" %c",186);	
+		//if(count<=3)
+		//{
+			printf("\n	%c",199); print(20,196); printf("%c",197); print(20,196); printf("%c",182);	
+		//	count++;
+		//}
+	}
+	printf("\n	%c",200); print(20,205); printf("%c",207); print(20,205); printf("%c",188);
+	//system("pause");	
+}	
---
+++ Main Files/Main File.cpp
@@ -0,0 +1,134 @@
+
+#include <time.h>       /* time_t, struct tm, difftime, time, mktime */
+#include<conio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+#include<math.h>
+#include<string.h>
+
+#include "Movement.h"
+#include "Highscores.h"
+using namespace std;
+
+void creategrid();
+void fillDigits();
+bool isValidSuduko();
+void startGame();
+
+int main()
+{
+	int choice = 1;
+	char current = 175;
+	char ch;
+	cout << "CPP SUDUKO!\n"; 
+	cout << (choice==1 ? current : ' ') << " START\n";
+	cout << (choice==2 ? current : ' ') << " HIGHSCORE";
+	while( (ch=getch()) != 13  )
+	{
+		system("cls");
+		switch(ch)
+		{
+			// Arrow Keys are pressed:
+			case 72 : if(choice>1) choice--;	break;
+			case 80 : if(choice<2) choice++;	break;
+		}	
+	
+		cout << "CPP SUDUKO!\n";
+		cout << (choice==1 ? current : ' ') << " START\n";
+		cout << (choice==2 ? current : ' ') << " HIGHSCORE";
+	}
+	
+	switch(choice)
+	{
+		case 1 : startGame(); break;
+		case 2 : printHighscores(); break;
+	}
+	
+	
+	return 0;
+}
+
+
+void startGame()
+{
+	time_t startTime, endTime, diffTime;
+	startTime = time (NULL);
+	
+	// Choosing an array ?
+	fillDigits();
+	
+	grid[x][y].bOpen = '[';
+	grid[x][y].bClose = ']';
+	
+	char ch;
+	while( (ch=getch()) != 27  )
+	{
+		system("cls");
+		//creategrid();
+		
+		switch(ch)
+		{
+			// Arrow Keys are pressed:
+			case 75 : moveLeft();	break;
+			case 77 : moveRight();	break;
+			case 72 : moveUp();	break;
+			case 80 : moveDown();	break;
+			
+			// Number Keys are pressed:
+			case '0' : if(grid[x][y].isEdit == 'T')
+						grid[x][y].digit = ' ';
+					   break;
+			case '1' : if(grid[x][y].isEdit == 'T')
+						grid[x][y].digit = '1';
+					   break;
+			case '2' : if(grid[x][y].isEdit == 'T')
+						grid[x][y].digit = '2';
+					   break;
+			case '3' : if(grid[x][y].isEdit == 'T')
+						grid[x][y].digit = '3';
+					   break;
+			case '4' : if(grid[x][y].isEdit == 'T')
+						grid[x][y].digit = '4';
+					   break;
+			case '5' : if(grid[x][y].isEdit == 'T')
+						grid[x][y].digit = '5';
+					   break;
+			case '6' : if(grid[x][y].isEdit == 'T')
+						grid[x][y].digit = '6';
+					   break;
+			case '7' : if(grid[x][y].isEdit == 'T')
+						grid[x][y].digit = '7';
+					   break;
+			case '8' : if(grid[x][y].isEdit == 'T')
+						grid[x][y].digit = '8';
+					   break;
+			case '9' : if(grid[x][y].isEdit == 'T')
+						grid[x][y].digit = '9';
+					   break;
+			
+			// Backspace:
+			case 8 :  if(grid[x][y].isEdit == 'T')
+						grid[x][y].digit = ' ';
+					   break;
+			
+			// Submit:
+			case 13 : if(!isValidSuduko())
+					  {
+					  	endTime = time (NULL);
+					  	diffTime = endTime - startTime;
+					  	cout <<"Correct and you took "<<diffTime;
+					  	// Entering the data to the file
+					  	
+					  		saveHighscore( diffTime );
+						return;	  	
+					  }	 
+					  else
+					  	cout <<"Wrong";
+					  break;	
+		}
+		
+		creategrid();
+		
+	}
+}
